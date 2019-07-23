4c8b3c912048f4faa60016536722338c0b07f0f9
okakyo <0622okakyo@gmail.com>
2019-07-23 21:55:29 +0900

    コードのまとめ修正

--- frontend/src/views/Search.vue
+++ frontend/src/views/Search.vue
@@ -1,15 +1,14 @@
 <template lang="pug">
     .search
         search-card
-        show
+
 </template>
 
 <script lang="ts">
     import SearchCard from "@/components/SearchCard.vue";
-    import show from "@/components/show.vue";
     export default {
         name: "Search",
-        components: {show, SearchCard}
+        components: { SearchCard}
     }
 </script>
 
